78c962e2c9d72dfd1c394dea9b20e6754e35d25a
VISHWANATH T S <31544005+vishwanath16@users.noreply.github.com>
2019-07-23 16:48:41 +0530

    Delete Color to Gray.py

--- Color to Gray.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-img = cv2.imread("photo.jpg", 0)
-
-resize_img = cv2.resize(img, (400, 600))
-cv2.imshow("Gray Image", resize_img)
-cv2.imwrite("Gray Image.jpg", resize_img)
-cv2.waitKey(0)
